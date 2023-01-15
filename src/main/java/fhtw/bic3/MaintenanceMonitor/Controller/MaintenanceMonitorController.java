95892724f8b4475c3a7c1ef856e622dac4d959c1
Ervin Sinanovic <ic22b019@technikum-wien.at>
2023-01-15 15:10:50 +0100

    adds functionality to index.html

--- src/main/java/fhtw/bic3/MaintenanceMonitor/Controller/MaintenanceMonitorController.java
+++ src/main/java/fhtw/bic3/MaintenanceMonitor/Controller/MaintenanceMonitorController.java
@@ -1,65 +1,29 @@
 package fhtw.bic3.MaintenanceMonitor.Controller;
-import fhtw.bic3.MaintenanceMonitor.Service.MaintenanceMonitorService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Controller class
  * REST functionality
  */
-@Controller
+@RestController
+@CrossOrigin
 public class MaintenanceMonitorController {
 
-    private final MaintenanceMonitorService monitorService;
 
-    public MaintenanceMonitorController(MaintenanceMonitorService monitorService) {
-        this.monitorService = monitorService;
+    public String message="0";
+
+    @GetMapping("/maintMode/")
+    public ResponseEntity<String> displayMessage() {
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
-    /**
-     * Basic test of Web
-     * @return home the home.html site
-     */
-    @GetMapping("/home")
-    public String home() {
-        return "home";  //home.html
+    @RequestMapping("/maintMessage/{message}")
+    public String writeMessage(@PathVariable("message") String message) {
+        this.message=message;
+        return (message.equals("0")) ? "You good g" : "Error occured. Message: "+message;
     }
 
-    /**
-     * The message view
-     * @param model basic spring model
-     * @return mes the mes.html site
-     */
-    @GetMapping("/message")
-    String getMes(Model model) {
-        String message = monitorService.getMessage();
-        model.addAttribute("message", message); //set the message for mes.html
-
-        if (message.equals("error")) {
-            model.addAttribute("color", "#ec1110"); //background red
-        } else {
-            model.addAttribute("color", "#129721"); //background green
-        }
-
-        return "mes";   //mes.html
-    }
-
-    /**
-     * The set message view
-     * @param message the set message
-     * @param model basic spring model
-     * @return change the change.html site
-     */
-    @GetMapping("/message/{message}")
-    String setMes(@PathVariable String message, Model model) {
-        if (!message.equals("reset")) {
-            monitorService.setMessage(message); //set message with given path variable
-        } else {
-            monitorService.resetMessage();  //reset message
-        }
-        model.addAttribute("setText","You just set the message to: \""+monitorService.getMessage()+"\"");   //set the text for change.html
-        return "change";    //change.html
-    }
 }

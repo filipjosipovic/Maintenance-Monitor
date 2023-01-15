a0f47e22fc2572d323d92c612820e76a53530e19
Ervin Sinanovic <ic22b019@technikum-wien.at>
2023-01-15 13:09:02 +0100

    created project
    initialized spring

---
+++ MaintenanceMonitor/MaintenanceMonitor/src/test/java/fhtw/bic3/MaintenanceMonitor/MaintenanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.bic3.MaintenanceMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
